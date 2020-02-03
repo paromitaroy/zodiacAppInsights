da79a0e955b23fa80280552c6d02a9fbbe7c62a4
nikkh <nick-9979@outlook.com>
2020-02-03 11:55:57 +0000

    Update test.sh

--- deployment_scripts/test.sh
+++ deployment_scripts/test.sh
@@ -2,5 +2,5 @@
 echo Hello from the script
 echo "First arg: $1"
 echo "Second arg: $2"
-echo "LIMONE_BASE_URL: $LIMONE_BASE_URL"
+echo "LIMONE_BASE_URL: $GITHUB_LIMONE_BASE_URL"
 # az webapp list --query "[?state=='Running']"
