aa3034cfd3ceccd63a5c933df10bc51a0c83e66e
nikkh <nick-9979@outlook.com>
2020-02-04 10:40:58 +0000

    Update deploy_sirmione_web.sh

--- deployment_scripts/deploy_sirmione_web.sh
+++ deployment_scripts/deploy_sirmione_web.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-echo Deploying sirmione-web...
-echo "I dont do anything"
-echo Global Variables...
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo         Deploying sirmione-web
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo >> Global Variables
 echo "SIRMIONE_ALIAS: $SIRMIONE_ALIAS"
 echo "DEFAULT_LOCATION: $DEFAULT_LOCATION"
 echo "DBADMIN_USER: $DBADMIN_USER"
 echo "DBADMIN_USER_PASSWORD_DO_DIFFERENTLY: $DBADMIN_USER_PASSWORD_DO_DIFFERENTLY"
-
+echo "LIMONE_ALIAS: $LIMONE_ALIAS"
+echo "SCORPIO_ALIAS: $SCORPIO_ALIAS_ALIAS"
+echo
 # set local variables
 appservice_webapp_sku="S1 Standard"
 database_edition="Basic"
-echo local Variables...
+echo >> Local Variables
 echo "App service Sku: $appservice_webapp_sku"
 echo "Database Edition: $database_edition"
-
+echo 
 # Derive as many variables as possible
 applicationName="${SIRMIONE_ALIAS}"
 webAppName="${applicationName}-web"
@@ -21,22 +24,27 @@ hostingPlanName="${applicationName}-plan"
 dbServerName="${applicationName}-db-server"
 dbName="${applicationName}-web-db"
 resourceGroupName="${applicationName}-rg"
+limoneBaseUrl="${LIMONE_ALIAS}-api.azurewebsites.net"
+scorpioBaseUrl="${SCORPIO_ALIAS}-api.azurewebsites.net"
 
-echo Derived Variables...
+echo >> Derived Variables
 echo "Application Name: $applicationName"
 echo "Resource Group Name: $resourceGroupName"
 echo "Web App Name: $webAppName"
 echo "Hosting Plan: $hostingPlanName"
 echo "DB Server Name: $dbServerName"
 echo "DB Name: $dbName"
+echo "Limone base url: $limoneBaseUrl"
+echo "Scorpio base url: $scorpioBaseUrl"
 echo
+
 echo "I dont do anything else"
 
-# az group create -l "$GITHUB_LOCATION" --n "$resourceGroupName" --tags  Application=$applicationName
+# az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=$applicationName
    
 # az group deployment create -g $resourceGroupName \
 #    --template-file sirmione-web/ArmTemplates/windows-webapp-sql-template.json  \
-#    --parameters webAppName=$webAppName hostingPlanName=$hostingPlanName appInsightsLocation=$GITHUB_LOCATION \
-#        databaseServerName=$dbServerName databaseUsername=$GITHUB_DB_USER databasePassword=$GITHUB_DB_PASSWORD databaseLocation=$GITHUB_LOCATION \
+#    --parameters webAppName=$webAppName hostingPlanName=$hostingPlanName appInsightsLocation=$DEFAULT_LOCATION \
+#        databaseServerName=$dbServerName databaseUsername=$DBADMIN_USER databasePassword=$DBADMIN_USER_PASSWORD_DO_DIFFERENTLY databaseLocation=$DEFAULT_LOCATION \
 #        databaseName=$dbName \
 #        sku="${appservice_webapp_sku}" databaseEdition=$database_edition
