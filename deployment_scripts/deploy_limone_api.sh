d8c58548e9434235c259c33f16226bc39ca7eecd
nikkh <nick-9979@outlook.com>
2020-02-04 13:08:39 +0000

    Update deploy_limone_api.sh

--- deployment_scripts/deploy_limone_api.sh
+++ deployment_scripts/deploy_limone_api.sh
@@ -37,7 +37,7 @@ echo "Storage account name: $storageAccountName"
 echo
 
 echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
-az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" Application=zodiac Micrososervice=$applicationName PendingDelete=true
+az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags Application=zodiac Micrososervice=$applicationName PendingDelete=true
 
 echo "Creating service bus namespace $serviceBusNamespace in group $resourceGroupName"
 az servicebus namespace create -g $scorpio_rg -n $scorpio_servicebus_namespace
