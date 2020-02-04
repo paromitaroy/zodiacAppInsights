e19cc2d193aec5566c5dfa8fb8032b97c97e5df0
nikkh <nick-9979@outlook.com>
2020-02-04 11:04:18 +0000

    Update deploy_scorpio_api.sh

--- deployment_scripts/deploy_scorpio_api.sh
+++ deployment_scripts/deploy_scorpio_api.sh
@@ -1,2 +1,46 @@
-echo Deploying scorpio-api...
-echo "I dont do anything"
+#!/bin/bash
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo         Deploying scorpio-api
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo ---Global Variables
+echo "SCORPIO_ALIAS: $SCORPIO_ALIAS"
+echo "DEFAULT_LOCATION: $DEFAULT_LOCATION"
+echo "DBADMIN_USER: $DBADMIN_USER"
+echo "DBADMIN_USER_PASSWORD_DO_DIFFERENTLY: $DBADMIN_USER_PASSWORD_DO_DIFFERENTLY"
+echo
+
+# set local variables
+appservice_webapp_sku="S1 Standard"
+
+echo ---Local Variables
+echo "App service Sku: $appservice_webapp_sku"
+echo 
+
+# Derive as many variables as possible
+applicationName="${SCORPIO_ALIAS}"
+webAppName="${applicationName}-api"
+hostingPlanName="${applicationName}-plan"
+resourceGroupName="${applicationName}-rg"
+
+dbServerName="${applicationName}-db-server"
+dbName="${applicationName}-web-db"
+
+echo ---Derived Variables
+echo "Application Name: $applicationName"
+echo "Resource Group Name: $resourceGroupName"
+echo "Web App Name: $webAppName"
+echo "Hosting Plan: $hostingPlanName"
+echo "DB Server Name: $dbServerName"
+echo "DB Name: $dbName"
+echo
+
+databaseConnectionString="Server=tcp:$dbServerName.database.windows.net;Database=$dbName;User ID=$DBADMIN_USER;Password=$DBADMIN_USER_PASSWORD_DO_DIFFERENTLY;Encrypt=True;Connection Timeout=30;"
+echo "Database connection string: $databaseConnectionString"
+echo "I dont do anything else"
+
+# az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=$applicationName
+   
+# az group deployment create -g $resourceGroupName \
+#    --template-file scorpio-api/ArmTemplates/windows-webapp-template.json  \
+#    --parameters webAppName=$webAppName hostingPlanName=$hostingPlanName appInsightsLocation=$DEFAULT_LOCATION \
+#        sku="${appservice_webapp_sku}" databaseConnectionString="{$databaseConnectionString}"
