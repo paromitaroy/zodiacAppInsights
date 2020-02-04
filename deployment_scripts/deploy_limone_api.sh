6ba3a23a845ac72fcd2876cc9f4daaae99b69434
nikkh <nick-9979@outlook.com>
2020-02-04 12:07:40 +0000

    Update deploy_limone_api.sh

--- deployment_scripts/deploy_limone_api.sh
+++ deployment_scripts/deploy_limone_api.sh
@@ -23,6 +23,8 @@ webAppName="${applicationName}-api"
 hostingPlanName="${applicationName}-plan"
 resourceGroupName="${applicationName}-rg"
 acrRegistryName="${applicationName}acr"
+serviceBusNamespace="${applicationName}-sb"
+storageAccountName=${applicationName}$RANDOM
 
 echo ---Derived Variables
 echo "Application Name: $applicationName"
@@ -30,17 +32,30 @@ echo "Resource Group Name: $resourceGroupName"
 echo "Web App Name: $webAppName"
 echo "Hosting Plan: $hostingPlanName"
 echo "ACR Name: $acrRegistryName"
+echo "Service Bus Namespace: $serviceBusNamespace"
+echo "Storage account name: $storageAccountName"
 echo
 
 echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
 # az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=$applicationName
 
-echo "Creating azure container registry $acrRegistryName in $DEFAULT_LOCATION"
+echo "Creating service bus namespace $serviceBusNamespace in group $resourceGroupName"
+# az servicebus namespace create -g $scorpio_rg -n $scorpio_servicebus_namespace
+
+echo "Creating storage account $storageAccountName in $resourceGroupName"
+# Create an Azure storage account in the resource group.
+# az storage account create \
+#  --name $storageAccountName \
+#  --location $DEFAULT_LOCATION \
+#  --resource-group $resourceGroupName \
+#  --sku Standard_LRS
+
+echo "Creating azure container registry $acrRegistryName in group $resourceGroupName"
 # az group deployment create -g $resourceGroupName \
 #    --template-file limone-api/ArmTemplates/containerRegistry-template.json  \
 #    --parameters registryName=$acrRegistryName registryLocation=$DEFAULT_LOCATION registrySku=$acrSku
 
-echo "Creating app service $webAppName in $DEFAULT_LOCATION"
+echo "Creating app service $webAppName in group $resourceGroupName"
 # az group deployment create -g $resourceGroupName \
 #    --template-file limone-api/ArmTemplates/container-webapp-template.json  \
 #    --parameters webAppName=$webAppName hostingPlanName=$hostingPlanName appInsightsLocation=$DEFAULT_LOCATION \
