43557288cb9bc043ce7666babc6cbc43c8138c5d
nikkh <nick-9979@outlook.com>
2020-02-04 16:30:09 +0000

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./deploy_sirmione_web.sh
-./deploy_scorpio_api.sh
-./deploy_limone_api.sh
-./deploy_virgo.sh
-./deploy_libra.sh
+deployment_scripts/deploy_sirmione_web.sh
+deployment_scripts/deploy_scorpio_api.sh
+deployment_scripts/deploy_limone_api.sh
+deployment_scripts/deploy_virgo.sh
+deployment_scripts/deploy_libra.sh
