6a0cbbf03bfe9a93692aad5901a4c56b1db85acc
nikkh <nick-9979@outlook.com>
2020-02-04 11:43:35 +0000

    Update deploy_virgo.sh

--- deployment_scripts/deploy_virgo.sh
+++ deployment_scripts/deploy_virgo.sh
@@ -1,2 +1,39 @@
-echo Deploying virgo...
-echo "I dont do anything"
+#!/bin/bash
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo         Deploying Virgo
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo ---Global Variables
+echo "VIRGO_ALIAS: $VIRGO_ALIAS"
+echo "DEFAULT_LOCATION: $DEFAULT_LOCATION"
+echo
+# set local variables
+# Derive as many variables as possible
+applicationName="${VIRGO_ALIAS}"
+resourceGroupName="${applicationName}-rg"
+storageAccountName=${applicationName}$RANDOM
+functionAppName=${applicationName-fa}
+
+echo ---Derived Variables
+echo "Application Name: $applicationName"
+echo "Resource Group Name: $resourceGroupName"
+echo "Storage Account Name: $storageAccountName"
+echo "Function App Name: $functionAppName"
+echo
+
+echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
+# az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=$applicationName
+
+echo "Creating storage account $storageAccountName in $resourceGroupName"
+# Create an Azure storage account in the resource group.
+# az storage account create \
+#  --name $storageAccountName \
+#  --location $DEFAULT_LOCATION \
+#  --resource-group $resourceGroupName \
+#  --sku Standard_LRS
+
+echo "Creating serverless function app $functionAppName in $resourceGroupName"
+# az functionapp create \
+#  --name $functionAppName \
+#  --storage-account $storageAccountName \
+#  --consumption-plan-location $DEFAULT_LOCATION \
+#  --resource-group $resourceGroupName
