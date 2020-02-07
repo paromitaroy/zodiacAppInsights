08ca2d8528b5be8ff275366b788d4c59ef933452
nhill <nhill@MININT-CU56C8E>
2020-02-07 13:13:32 +0000

    Bring in fro original project - changes were made there

--- scorpio-api/Application/Controllers/AriesController.cs
+++ scorpio-api/Application/Controllers/AriesController.cs
@@ -24,7 +24,7 @@ namespace scorpio_api.Controllers
          
          /// <summary>
          /// Very simple API.  Just returns Hello World.  Also logs error, warning, infomration, debug and trace output
-         /// </summary> 
+         /// </summary>
          /// <returns>a string containing hello world</returns>
         [HttpGet]
         public ActionResult<string> Get(string traceGuid)
@@ -36,7 +36,7 @@ namespace scorpio_api.Controllers
             _telemetryClient.TrackEvent(message);
             _telemetryClient.GetMetric(metricName).TrackValue(1);
             _logger.LogInformation(message);
-            return "Hello World from Aries!v";
+            return "Hello World from Aries!";
         }
 
        
---
+++ scorpio-api/Application/Controllers/SettingsViewController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace scorpio_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsViewController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly TelemetryClient _telemetryClient;
+        private readonly IConfiguration _configuration;
+        private List<string> _results = new List<string>();
+
+        public SettingsViewController(ILogger<SettingsViewController> logger, TelemetryClient telemetryClient, IConfiguration configuration)
+        {
+            _logger = logger;
+            _telemetryClient = telemetryClient;
+            _configuration = configuration;
+            
+        }
+
+        private void LogString(string name, string stringToLog)
+        {
+            _results.Add($"{name}:{stringToLog}");
+            _telemetryClient.TrackTrace($"{name}:{stringToLog}", SeverityLevel.Information);
+            Console.WriteLine($"{name}:{stringToLog}");
+            _logger.LogDebug($"{name}:{stringToLog}");
+        }
+        // GET: api/SettingsView
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+ 
+            string myCustomKeyFormatA = _configuration["MyCustomKey"];
+            LogString("myCustomKeyFormatA", myCustomKeyFormatA);
+
+            string myCompoundCustomKeyElement1FormatA = _configuration["MyCompoundCustomKey:Element1"];
+            LogString("myCompoundCustomKeyElement1FormatA", myCompoundCustomKeyElement1FormatA);
+
+            string myCompoundCustomKeyElement2FormatA = _configuration["MyCompoundCustomKey:Element2"];
+            LogString("myCompoundCustomKeyElement2FormatA", myCompoundCustomKeyElement2FormatA);
+
+            var dbConnectionStringFormatA = _configuration["Azure:a3ssDevDb:ConnectionString"];
+            LogString("dbConnectionStringFormatA", dbConnectionStringFormatA);
+
+            return _results;
+        }
+
+        
+    }
+}
