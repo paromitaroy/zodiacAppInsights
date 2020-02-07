9d7b219857f494b4ca4152a3e00aa8ae1b71fb6e
nikkh <nick-9979@outlook.com>
2020-02-07 14:54:23 +0000

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-deployment_scripts/set_environment.sh
+source deployment_scripts/set_environment.sh
 deployment_scripts/deploy_sirmione_web.sh
 deployment_scripts/deploy_scorpio_api.sh
 deployment_scripts/deploy_limone_api.sh
