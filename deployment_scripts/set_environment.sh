0ef359ac2ad53b6b0c0177e34987f3fafa0c3300
nikkh <nick-9979@outlook.com>
2020-03-31 16:41:36 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export DEFAULT_LOCATION=eastus
-export SIRMIONE_ALIAS=firmione
-export LIMONE_ALIAS=fimone
-export SCORPIO_ALIAS=forpio
-export VIRGO_ALIAS=firgo
-export LIBRA_ALIAS=fibra
+export DEFAULT_LOCATION=centralus
+export SIRMIONE_ALIAS=xsirmione
+export LIMONE_ALIAS=xlimone
+export SCORPIO_ALIAS=xscorpio
+export VIRGO_ALIAS=xvirgo
+export LIBRA_ALIAS=xlibra
 export DB_ADMIN_USER=nick
