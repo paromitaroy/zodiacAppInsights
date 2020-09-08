e6499936cc12c4d1286601d65ff32ee3e6b7c5af
nhill <nhill@MININT-CU56C8E>
2020-09-08 17:00:39 +0100

    added id tags on buttons for selenium

--- Zodiac.Generator/UI/UIWorker.cs
+++ Zodiac.Generator/UI/UIWorker.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Internal;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -36,8 +38,41 @@ namespace Zodiac.Generator.UI
         internal async Task<string> Run(ILogger log, string triggerFunction, int calls = 0)
         {
             driver.Navigate().GoToUrl("https://sirmione-web.azurewebsites.net/");
-            var links = driver.FindElements(By.TagName("a"));
-            return links.ToString(); ;
+            var capricornLink = driver.FindElement(By.Id("action-Capricorn"));
+            capricornLink.Click();
+            var emailbox = driver.FindElement(By.Id("i0116"));
+            log.LogTrace($"Found emailbox {emailbox.Text}");
+            emailbox.SendKeys("neves@xekina.onmicrosoft.com");
+            log.LogTrace($"Typed email address {emailbox.Text}");
+            var nextButton = driver.FindElement(By.Id("idSIButton9"));
+            log.LogTrace($"Found Next Button");
+            nextButton.Click();
+            log.LogTrace($"Clicked Next Button");
+            var passwordBox = driver.FindElement(By.Id("i0118"));
+            log.LogTrace($"Found passwordBox");
+            passwordBox.SendKeys("Boldmere1883@@@");
+            log.LogTrace("Typed Password");
+            var signinButton = driver.FindElement(By.Id("idSIButton9"));
+            log.LogTrace($"Found signinButton");
+
+            signinButton.Click();
+            log.LogTrace($"signinButton clicked");
+            try
+            {
+                var staySignedIn = driver.FindElement(By.Id("idBtn_Back"));
+                log.LogTrace($"Found idBtnBack");
+                staySignedIn.Click();
+                log.LogTrace("clicked stay signed in");
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            var goButton = driver.FindElement(By.ClassName("btn btn-primary"));
+            log.LogTrace($"Found goButton");
+            goButton.Click();
+            return "ok";
         }
     }
 }
