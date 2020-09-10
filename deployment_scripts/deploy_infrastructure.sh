92e6b3456d4c388f1857d7e0093b4b6c11fb94fd
nikkh <nick-9979@outlook.com>
2020-09-10 13:47:43 +0100

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo starting >> deployment-log.txt
 source deployment_scripts/set_environment.sh
 deployment_scripts/deploy_sirmione_web.sh
 deployment_scripts/deploy_scorpio_api.sh
@@ -6,3 +7,5 @@ deployment_scripts/deploy_limone_api.sh
 deployment_scripts/deploy_virgo.sh
 deployment_scripts/deploy_libra.sh
 deployment_scripts/deploy_zodiac_generator.sh
+echo finished >> deployment-log.txt
+cat deployment-log.txt
