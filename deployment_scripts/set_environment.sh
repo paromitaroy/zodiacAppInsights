cab3020c96d0b6fdc9e809e9f9219ea47e4c19aa
nikkh <nick-9979@outlook.com>
2020-09-10 10:05:36 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -6,6 +6,6 @@ export LIMONE_ALIAS=limonez
 export SCORPIO_ALIAS=scorpioz
 export VIRGO_ALIAS=virgoz
 export LIBRA_ALIAS=libraz
-export ZODIAC_GENERATOR_ALIAS=azodgen
+export ZODIAC_GENERATOR_ALIAS=zodgenz
 # database needs to be self contained in this deployment
 export DB_ADMIN_USER=nick
