c94b5e6dfa3e12310f52b2cff29bb2b3c67bde7d
nikkh <nick-9979@outlook.com>
2020-09-10 14:12:59 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=sirmionek
-export LIMONE_ALIAS=limonek
-export SCORPIO_ALIAS=scorpiok
-export VIRGO_ALIAS=virgok
-export LIBRA_ALIAS=librak
-export ZODIAC_GENERATOR_ALIAS=zodgenk
-# database needs to be self contained in this deployment
+export SIRMIONE_ALIAS=sirmionea
+export LIMONE_ALIAS=limonea
+export SCORPIO_ALIAS=scorpioa
+export VIRGO_ALIAS=virgoa
+export LIBRA_ALIAS=libraa
+export ZODIAC_GENERATOR_ALIAS=zodgena
 export DB_ADMIN_USER=nick
+
+echo "DEFAULT_LOCATION:$DEFAULT_LOCATION" >> deployment-log.txt
+echo "SIRMIONE_ALIAS:$SIRMIONE_ALIAS" >> deployment-log.txt
+echo "LIMONE_ALIAS:$LIMONE_ALIAS" >> deployment-log.txt
+echo "SCORPIO_ALIAS:$SCORPIO_ALIAS" >> deployment-log.txt
+echo "VIRGO_ALIAS:$VIRGO_ALIAS" >> deployment-log.txt
+echo "LIBRA_ALIAS:$LIBRA_ALIAS" >> deployment-log.txt
+echo "ZODIAC_GENERATOR_ALIAS:$ZODIAC_GENERATOR_ALIAS" >> deployment-log.txt
+echo "DB_ADMIN_USER:$DB_ADMIN_USER" >> deployment-log.txt
