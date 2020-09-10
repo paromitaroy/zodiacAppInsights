858c3b57055094e170caa959cc952d646c3292b6
nikkh <nick-9979@outlook.com>
2020-09-10 15:16:06 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=sirmioneb
-export LIMONE_ALIAS=limoneb
-export SCORPIO_ALIAS=scorpiob
-export VIRGO_ALIAS=virgob
-export LIBRA_ALIAS=librab
-export ZODIAC_GENERATOR_ALIAS=zodgenb
+export SIRMIONE_ALIAS=sirmionec
+export LIMONE_ALIAS=limonec
+export SCORPIO_ALIAS=scorpioc
+export VIRGO_ALIAS=virgoc
+export LIBRA_ALIAS=librac
+export ZODIAC_GENERATOR_ALIAS=zodgenc
 export DB_ADMIN_USER=nick
 
 echo "DEFAULT_LOCATION:$DEFAULT_LOCATION" >> deployment-log.txt
