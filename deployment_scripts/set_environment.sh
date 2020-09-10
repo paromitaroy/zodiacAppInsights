3239f88603756e540eef61274436a678ea2abcb6
nikkh <nick-9979@outlook.com>
2020-09-10 13:49:48 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=sirmionex
-export LIMONE_ALIAS=limonex
-export SCORPIO_ALIAS=scorpiox
-export VIRGO_ALIAS=virgox
-export LIBRA_ALIAS=librax
-export ZODIAC_GENERATOR_ALIAS=zodgenx
+export SIRMIONE_ALIAS=sirmionek
+export LIMONE_ALIAS=limonek
+export SCORPIO_ALIAS=scorpiok
+export VIRGO_ALIAS=virgok
+export LIBRA_ALIAS=librak
+export ZODIAC_GENERATOR_ALIAS=zodgenk
 # database needs to be self contained in this deployment
 export DB_ADMIN_USER=nick
