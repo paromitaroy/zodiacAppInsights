a7fbe729a7ce4aacca0611bfdb29605260f0d2a7
nikkh <nick-9979@outlook.com>
2020-09-10 15:09:48 +0100

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -8,8 +8,26 @@ deployment_scripts/deploy_virgo.sh
 deployment_scripts/deploy_libra.sh
 deployment_scripts/deploy_zodiac_generator.sh
 echo finished >> deployment-log.txt
-cat deployment-log.txt
+
 blobName="deployment-log.txt"
+cat $blobName
+
+resourceGroupName="${ZODIAC_GENERATOR_ALIAS}-rg"
+echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
+az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=zodiac Micrososervice=$applicationName PendingDelete=true
+
+storageAccountName="${ZODIAC_GENERATOR_ALIAS}$RANDOM"
+echo "Creating storage account $storageAccountName in $resourceGroupName"
+az storage account create \
+ --name $storageAccountName \
+ --location $DEFAULT_LOCATION \
+ --resource-group $resourceGroupName \
+ --sku Standard_LRS
+
+# We'll use this storage account to hold the log and secrets generated during infrastructure creation.
+connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
+export AZURE_STORAGE_CONNECTION_STRING=$connectionString
+
 az storage container create -n "results" --public-access off
 az storage blob upload -c "results" -f $blobName -n $blobName
 expiry=$(date --date="1 day" +%F)
