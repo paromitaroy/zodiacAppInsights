df344d19240ea18d85b3915d46ae278667882a6d
nikkh <nick-9979@outlook.com>
2020-09-11 16:25:31 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=sirmionef
-export LIMONE_ALIAS=limonef
-export SCORPIO_ALIAS=scorpiof
-export VIRGO_ALIAS=virgof
-export LIBRA_ALIAS=libraf
-export ZODIAC_GENERATOR_ALIAS=zodgenf
+export SIRMIONE_ALIAS=sirmioneg
+export LIMONE_ALIAS=limoneg
+export SCORPIO_ALIAS=scorpiog
+export VIRGO_ALIAS=virgog
+export LIBRA_ALIAS=librag
+export ZODIAC_GENERATOR_ALIAS=zodgeng
 export DB_ADMIN_USER=nick
 echo "Creating Infrastructure using the following environment variables" >> deployment-log.txt
 echo "DEFAULT_LOCATION:$DEFAULT_LOCATION" >> deployment-log.txt
