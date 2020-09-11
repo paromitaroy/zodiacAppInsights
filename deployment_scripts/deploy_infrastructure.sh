b6060f17dc175e21f1f36f2f6617cfff97e5faa5
nikkh <nick-9979@outlook.com>
2020-09-11 15:54:32 +0100

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -24,6 +24,11 @@ az storage account create \
  --resource-group $resourceGroupName \
  --sku Standard_LRS
 
+connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
+export AZURE_STORAGE_CONNECTION_STRING="$connectionString"
+echo "Generator storage account: $storageAccountName" >> deployment-log.txt
+echo "Generator storage account connection string: $connectionString" >> deployment-log.txt
+
 # We'll use this storage account to hold external configuration for users and sessions in user simulation processing
 az storage container create -n "zodiac-generator-config" --public-access off
 sampleGeneratorParameters="{"Users": [{"Id": "user1@tenant.onmicrosoft.com", "Password": "password"},{"Id": "user2@tenant.onmicrosoft.com","Password": "password"}],"Sessions": [{"Steps": ["capricorn-go-red", "cap021", "cap023", "cap024" ] }, { "Steps": [ "capricorn-go-rainbow", "cap013", "cap019", "cap006" ] },{ "Steps": [ "capricorn-go-blue", "cap003" ] }]}"
@@ -32,15 +37,19 @@ az storage blob upload -c "zodiac-generator-config" -f GeneratorParameters.json 
 echo "GeneratorParameters.json was written to $storageAccountName, container=zodiac-generator-config, blob=GeneratorParameters.json" >> deployment-log.txt
 
 # We'll use this storage account to hold the log and secrets generated during infrastructure creation.
-connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
-export AZURE_STORAGE_CONNECTION_STRING=$connectionString
-echo "Generator storage account: $storageAccountName" >> deployment-log.txt
-echo "Generator storage account connection string: $connectionString" >> deployment-log.txt
+
 echo finished >> deployment-log.txt
 
 az storage container create -n "results" --public-access off
 az storage blob upload -c "results" -f $blobName -n $blobName
+today=$(date +%F)T
+startTime=$(date --date="-1 hour" +%T)T
+expiryTime=$(date --date="2 hour" +%T)T
+start="$today$startTime"
+expiry="$today$expiryTime"
+echo $start
 expiry=$(date --date="1 day" +%F)
+echo $expiry
 url=$(az storage blob url -c "results" -n $blobName -o tsv)
-sas=$(az storage blob generate-sas -c "private" -n $blobName --permissions r -o tsv --expiry $expiry)
+sas=$(az storage blob generate-sas -c "private" -n $blobName --permissions r -o tsv --expiry $expiry --https-only --start $start)
 echo "Click here to access a concise list of parameter that may be useful: $url?$sas".
