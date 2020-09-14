89bbff2fde3b157907fbafd170f7b3561327789c
nikkh <nick-9979@outlook.com>
2020-09-14 17:04:58 +0100

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -7,26 +7,27 @@ deployment_scripts/deploy_limone_api.sh
 deployment_scripts/deploy_virgo.sh
 deployment_scripts/deploy_libra.sh
 deployment_scripts/deploy_zodiac_generator.sh
+echo finished >> deployment-log.txt
 
 echo ">>>> Temporary"
 blobName="deployment-log.txt"
 cat $blobName
 echo "<<<< Temporary"
 
-
-# We'll use this storage account to hold the log and secrets generated during infrastructure creation.
-
-echo finished >> deployment-log.txt
-
+# Upload the deployment log to the zodiac storage account 
+resourceGroupName="$ZODIAC_ALIAS-rg"
+storageAccountName=$(az storage account list -g zodgeng-rg --query [0].name -o tsv)
+connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
 az storage container create -n "results" --public-access off
 az storage blob upload -c "results" -f $blobName -n $blobName
+
+# Generate a SAS Token for direct access to the deployment log
 today=$(date +%F)T
 startTime=$(date --date="-1 hour" +%T)Z
-expiryTime=$(date --date="2 hour" +%T)Z
+expiryTime=$(date --date="1 hour" +%T)Z
 start="$today$startTime"
 expiry="$today$expiryTime"
 echo $start
-expiry=$(date --date="1 day" +%F)
 echo $expiry
 url=$(az storage blob url -c "results" -n $blobName -o tsv)
 sas=$(az storage blob generate-sas -c "private" -n $blobName --permissions r -o tsv --expiry $expiry --https-only --start $start)
