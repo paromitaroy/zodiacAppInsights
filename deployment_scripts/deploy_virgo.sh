1e0db43359875ae81184634ba5b7bb4b2a7f69a5
nikkh <nick-9979@outlook.com>
2020-09-14 17:32:56 +0100

    Update deploy_virgo.sh

--- deployment_scripts/deploy_virgo.sh
+++ deployment_scripts/deploy_virgo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Starting Virgo Deploy..." >> deployment-log.txt
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo         Deploying Virgo
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -27,16 +28,19 @@ echo "Function App Name: $functionAppName"
 echo
 
 echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
+echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION" >> deployment-log.txt
 az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=zodiac Micrososervice=$applicationName PendingDelete=true
 
 echo "Creating storage account $storageAccountName in $resourceGroupName"
+echo "Creating storage account $storageAccountName in $resourceGroupName" >> deployment-log.txt
 az storage account create \
 --name $storageAccountName \
 --location $DEFAULT_LOCATION \
 --resource-group $resourceGroupName \
 --sku Standard_LRS
 
-echo "Creating serverless function app $functionAppName in $resourceGroupName"
+echo "Creating function app $functionAppName in $resourceGroupName"
+echo "Creating function app $functionAppName in $resourceGroupName" >> deployment-log.txt
 az functionapp create \
  --name $functionAppName \
  --storage-account $storageAccountName \
@@ -45,4 +49,7 @@ az functionapp create \
  --functions-version 3
 
 echo "Updating App Settings for $functionAppName"
-az webapp config appsettings set -g $resourceGroupName -n $functionAppName --settings ServiceBusConnection=$limoneServiceBusConnectionString
+settings="ServiceBusConnection=$limoneServiceBusConnectionString"
+az webapp config appsettings set -g $resourceGroupName -n $functionAppName --settings settings -o none
+echo "Update settings for function app $functionAppName: $settings" >> deployment-log.txt
+echo "Virgo Deploy has completed." >> deployment-log.txt
