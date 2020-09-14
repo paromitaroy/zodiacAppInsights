522217b1617d089e7dab60de22bdabe5c347d430
nikkh <nick-9979@outlook.com>
2020-09-14 16:50:06 +0100

    Update deploy_infrastructure.sh

--- deployment_scripts/deploy_infrastructure.sh
+++ deployment_scripts/deploy_infrastructure.sh
@@ -13,31 +13,7 @@ blobName="deployment-log.txt"
 cat $blobName
 echo "<<<< Temporary"
 
-echo "TODO: Move this to the Zodiac generator script!"
-resourceGroupName="${ZODIAC_GENERATOR_ALIAS}-rg"
-echo "Creating resource group $resourceGroupName in $DEFAULT_LOCATION"
-az group create -l "$DEFAULT_LOCATION" --n "$resourceGroupName" --tags  Application=zodiac Micrososervice=$applicationName PendingDelete=true -o none
 
-storageAccountName="${ZODIAC_GENERATOR_ALIAS}$RANDOM"
-echo "Creating storage account $storageAccountName in $resourceGroupName"
-az storage account create \
- --name $storageAccountName \
- --location $DEFAULT_LOCATION \
- --resource-group $resourceGroupName \
- --sku Standard_LRS -o none
-
-connectionString=$(az storage account show-connection-string -n $storageAccountName -g $resourceGroupName --query connectionString -o tsv)
-export AZURE_STORAGE_CONNECTION_STRING="$connectionString"
-echo "Generator storage account: $storageAccountName" >> deployment-log.txt
-echo "Generator storage account connection string: $connectionString" >> deployment-log.txt
-
-# We'll use this storage account to hold external configuration for users and sessions in user simulation processing
-az storage container create -n "zodiac-generator-config" --public-access off
-sampleGeneratorParameters="{"Users": [{"Id": "user1@tenant.onmicrosoft.com", "Password": "password"},{"Id": "user2@tenant.onmicrosoft.com","Password": "password"}],"Sessions": [{"Steps": ["capricorn-go-red", "cap021", "cap023", "cap024" ] }, { "Steps": [ "capricorn-go-rainbow", "cap013", "cap019", "cap006" ] },{ "Steps": [ "capricorn-go-blue", "cap003" ] }]}"
-echo "$sampleGeneratorParameters" > GeneratorParameters.json
-az storage blob upload -c "zodiac-generator-config" -f GeneratorParameters.json -n GeneratorParameters.json
-echo "GeneratorParameters.json was written to $storageAccountName, container=zodiac-generator-config, blob=GeneratorParameters.json" >> deployment-log.txt
-echo "!! You will need to edit GeneratorParameters.json" >> deployment-log.txt
 # We'll use this storage account to hold the log and secrets generated during infrastructure creation.
 
 echo finished >> deployment-log.txt
