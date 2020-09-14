4b7ab0af5aeefb6f5d9db93549bac47e287c010b
nikkh <nick-9979@outlook.com>
2020-09-14 16:44:53 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -6,7 +6,7 @@ export LIMONE_ALIAS=limoneg
 export SCORPIO_ALIAS=scorpiog
 export VIRGO_ALIAS=virgog
 export LIBRA_ALIAS=librag
-export ZODIAC_GENERATOR_ALIAS=zodgeng
+export ZODIAC_ALIAS=zodgeng
 export DB_ADMIN_USER=nick
 echo "Creating Infrastructure using the following environment variables" >> deployment-log.txt
 echo "DEFAULT_LOCATION:$DEFAULT_LOCATION" >> deployment-log.txt
@@ -15,5 +15,5 @@ echo "LIMONE_ALIAS:$LIMONE_ALIAS" >> deployment-log.txt
 echo "SCORPIO_ALIAS:$SCORPIO_ALIAS" >> deployment-log.txt
 echo "VIRGO_ALIAS:$VIRGO_ALIAS" >> deployment-log.txt
 echo "LIBRA_ALIAS:$LIBRA_ALIAS" >> deployment-log.txt
-echo "ZODIAC_GENERATOR_ALIAS:$ZODIAC_GENERATOR_ALIAS" >> deployment-log.txt
+echo "ZODIAC_ALIAS:$ZODIAC_ALIAS" >> deployment-log.txt
 echo "DB_ADMIN_USER:$DB_ADMIN_USER" >> deployment-log.txt
