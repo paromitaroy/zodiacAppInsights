f3ccde56ad9a217953d82f20f594977c842de555
nikkh <nick-9979@outlook.com>
2020-09-16 18:10:00 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=zsirmione
-export LIMONE_ALIAS=zlimone
-export SCORPIO_ALIAS=zscorpio
-export VIRGO_ALIAS=zvirgo
-export LIBRA_ALIAS=zlibra
-export ZODIAC_ALIAS=zzodiac
+export SIRMIONE_ALIAS=xsirmione
+export LIMONE_ALIAS=xlimone
+export SCORPIO_ALIAS=xscorpio
+export VIRGO_ALIAS=xvirgo
+export LIBRA_ALIAS=xlibra
+export ZODIAC_ALIAS=xzodiac
 export DB_ADMIN_USER=nick
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
