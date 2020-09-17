45f42996f1f0013638e058f4d95746e38885b573
nikkh <nick-9979@outlook.com>
2020-09-17 13:56:12 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -11,8 +11,8 @@ export DB_ADMIN_USER=nick
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
-echo "<h1>Environment Variables</h1>" >> deployment-log.html
-echo "DEFAULT_LOCATION:$DEFAULT_LOCATION" >> deployment-log.html
+echo "<h2>Environment Variables</h2>" >> deployment-log.html
+echo "<p>DEFAULT_LOCATION:$DEFAULT_LOCATION</p>" >> deployment-log.html
 echo "SIRMIONE_ALIAS:$SIRMIONE_ALIAS" >> deployment-log.html
 echo "LIMONE_ALIAS:$LIMONE_ALIAS" >> deployment-log.html
 echo "SCORPIO_ALIAS:$SCORPIO_ALIAS" >> deployment-log.html
@@ -20,3 +20,4 @@ echo "VIRGO_ALIAS:$VIRGO_ALIAS" >> deployment-log.html
 echo "LIBRA_ALIAS:$LIBRA_ALIAS" >> deployment-log.html
 echo "ZODIAC_ALIAS:$ZODIAC_ALIAS" >> deployment-log.html
 echo "DB_ADMIN_USER:$DB_ADMIN_USER" >> deployment-log.html
+echo "AAD_DOMAIN:$AAD_DOMAIN" >> deployment-log.html
