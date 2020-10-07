a84ab99f10c693e2275c20ec75364106c979d79b
nikkh <nick-9979@outlook.com>
2020-10-07 15:49:01 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -11,9 +11,10 @@ export DB_ADMIN_USER=nick
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
+export OUTPUT=json
 
 # Whatever you set zodiac instance to will be tagged onto your azure resources, and enable you to access all the different components as a cohesive set
-export ZODIAC_INSTANCE=Sandman
+export ZODIAC_INSTANCE=Jemima
 
 echo "<h2>Environment Variables</h2>" >> deployment-log.html
 echo "<p>ZODIAC_INSTANCE:$ZODIAC_INSTANCE</p>" >> deployment-log.html
@@ -26,3 +27,4 @@ echo "LIBRA_ALIAS:$LIBRA_ALIAS" >> deployment-log.html
 echo "ZODIAC_ALIAS:$ZODIAC_ALIAS" >> deployment-log.html
 echo "DB_ADMIN_USER:$DB_ADMIN_USER" >> deployment-log.html
 echo "AAD_DOMAIN:$AAD_DOMAIN" >> deployment-log.html
+echo "OUTPUT:$OUTPUT" >> deployment-log.html
